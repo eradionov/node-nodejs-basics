93bc23d2dbc5702d74c1d86adc36a50c5f4d94a6
eugene.radionov <eugene.radionov@jagaad.it>
2023-06-13 11:01:54 +0200

    feat: implement hashing

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import {createReadStream} from "fs";
+import {dirname} from "../utils/dir.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const { createHash } = await import('node:crypto');
+
+    const __dirname = dirname(import.meta.url);
+    const hash = createHash('sha256');
+    const readStream = createReadStream(`${__dirname}/files/fileToCalculateHashFor.txt`);
+
+    readStream.pipe(hash)
+        .setEncoding('hex')
+        .pipe(process.stdout);
 };
 
 await calculateHash();
