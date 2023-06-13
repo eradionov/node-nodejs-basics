6cf391d80258b80fdfec1c9f01a3d55505d3c554
eugene.radionov <eugene.radionov@jagaad.it>
2023-06-13 11:13:53 +0200

    feat: implement file compression/decompression

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { pipeline } from 'stream/promises';
+import zlib from "node:zlib";
+import fs from "fs";
+import {dirname} from "../utils/dir.js";
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = dirname(import.meta.url);
+
+    try {
+        await pipeline(
+            fs.createReadStream(`${__dirname}/files/fileToCompress.txt`),
+            zlib.createGzip(),
+            fs.createWriteStream(`${__dirname}/files/archive.gz`)
+        );
+    } catch (error) {
+        throw new Error('Compression finished with error');
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import {pipeline} from "stream/promises";
+import fs from "fs";
+import zlib from "node:zlib";
+import {dirname} from "../utils/dir.js";
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = dirname(import.meta.url);
+
+    try {
+        await pipeline(
+            fs.createReadStream(`${__dirname}/files/archive.gz`),
+            zlib.createGunzip(),
+            fs.createWriteStream(`${__dirname}/files/fileToCompress.txt`)
+        );
+    } catch (error) {
+        throw new Error('Decompression finished with error');
+    }
 };
 
 await decompress();
